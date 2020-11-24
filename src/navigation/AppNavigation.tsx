c48f50e0037ffc18ecbf43764656d6c739f9f970
Artsiom Yermakou <artem_ermakov_1999@mail.ru>
2020-11-24 10:54:08 +0300

    Add cross platform styles

--- src/navigation/AppNavigation.tsx
+++ src/navigation/AppNavigation.tsx
@@ -3,15 +3,21 @@ import {MainScreen} from "../screens/MainScreen";
 import {PostScreen} from "../screens/PostScreen";
 import React from "react";
 import {NavigationContainer} from "@react-navigation/native";
+import {THEME} from "../theme";
+import {Platform} from "react-native";
 
 const Stack = createStackNavigator();
 
 export function MyStack() {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName={"MainScreen"}>
-                <Stack.Screen name="MainScreen" component={MainScreen}/>
-                <Stack.Screen name="PostScreen" component={PostScreen}/>
+            <Stack.Navigator initialRouteName={"MainScreen"}
+                             screenOptions={{
+                                 headerStyle: {backgroundColor: Platform.OS === "android" ? THEME.MAIN_COLOR : "#fff"},
+                                 headerTintColor: Platform.OS === "android" ? "#fff" : THEME.MAIN_COLOR
+                             }}>
+                <Stack.Screen name="MainScreen" component={MainScreen} options={{title: "My blog"}}/>
+                <Stack.Screen name="PostScreen" component={PostScreen} options={{title: "Posts"}}/>
             </Stack.Navigator>
         </NavigationContainer>
     );
